256e4b4ea9034350408c9866a16b657d57b0f2d1
Sydney Ho <sh967@cornell.edu>
2022-10-16 18:32:01 -0400

    Rework webscrape.ml - TODO: Fix No SSL or TLS support compiled into Conduit exception

--- src/webscrape.ml
+++ src/webscrape.ml
@@ -1,26 +1,24 @@
 open Mechaml
 module M = Agent.Monad
-open M.Infix
+open M.Syntax
 
 let require msg = function
   | Some a -> a
   | None -> failwith msg
 
 let search =
-  Agent.get "https://us.megabus.com/"
-  >|= (fun result ->
-        let form =
-          result |> Agent.HttpResponse.page
-          |> Page.form_with "[action$=search]"
-          |> require "search form not found"
-        in
-        let field =
-          form |> Page.Form.field_with "[name=q]" |> require "q feld not found"
-        in
-        Page.Form.Field.set form field "module Form")
-  >>= Agent.submit
-  >>= fun response ->
+  let* response = Agent.get "https://us.megabus.com/" in
+  let form =
+    response |> Agent.HttpResponse.page
+    |> Page.form_with "[action$=search]"
+    |> require "search form not found"
+  in
+  let field =
+    form |> Page.Form.field_with "[name=q]" |> require "q feld not found"
+  in
+  let form = Page.Form.Field.set form field "module Form" in
+  let* response = Agent.submit form in
   response |> Agent.HttpResponse.content
   |> M.save_content "megabus-search-result.html"
 
-let _ = M.run (Agent.init ()) search
+(* let _ = M.run (Agent.init ()) search *)
