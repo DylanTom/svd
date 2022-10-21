acee6a44e3b0400b4508680b7cc15d637b1171f1
Vanessa Fang <vf72@cornell.edu>
2022-10-21 02:40:20 -0400

    Created bus.ml
    - handles loading the json file
    - create the types based on the json file
    - checks for valid bus company
    - creates route_from and route_destination list
    - documentation for neccessary functions that need to be implemented

--- src/bus.ml
+++ src/bus.ml
@@ -1,13 +1,17 @@
 open Yojson.Basic.Util
+
+exception UnknownCompany of string
 exception UnknownCity of string
 
 type route = {
   from : string;
   destination : string;
-  month : string;
-  date : string;
-  year : string;
-  price : string;
+  month : int;
+  date : int;
+  year : int;
+  price : int;
+  timeleave : string;
+  timearrive : string;
   url : string;
 }
 
@@ -22,10 +26,12 @@ let route_of_json json =
   {
     from = json |> member "from" |> to_string;
     destination = json |> member "destination" |> to_string;
-    month = json |> member "month" |> to_string;
-    date = json |> member "date" |> to_string;
-    year = json |> member "year" |> to_string;
-    price = json |> member "price" |> to_string;
+    month = json |> member "month" |> to_int;
+    date = json |> member "date" |> to_int;
+    year = json |> member "year" |> to_int;
+    price = json |> member "price" |> to_int;
+    timeleave = json |> member "timeleave" |> to_string;
+    timearrive = json |> member "timearrive" |> to_string;
     url = json |> member "url" |> to_string;
   }
 
@@ -38,8 +44,41 @@ let buses_of_json json =
 let from_json json =
   { buses = json |> member "buses" |> to_list |> List.map buses_of_json }
 
-(* [find_city] is a recursive function that return true if the input [city] is in the [lst]. *)
-let rec find_city city lst =
+(** [find_company] is a recursive function that takes the inputs [company lst].
+    It will raise an UnknownCompany exception if the company is not in the list.
+    If it does exist in the list, it return the company in json format.*)
+let rec find_company company lst =
   match lst with
-  | [] -> false
-  | h :: t -> if h.from = city then true else find_city city t
+  | [] -> raise (UnknownCompany company)
+  | h :: t -> if h.company = company then h else find_company company t
+
+(** [route_from_list] is a recursive function that takes the input [lst] of type
+    buses list and outputs a list of the route from type with the type string
+    list*)
+let rec route_from_list lst =
+  match lst with
+  | [] -> []
+  | h :: t -> h.from :: route_from_list t
+
+(** [route_destination_list] is a recursive function that takes the input [lst]
+    of type buses list and outputs a list of the route destination type with the
+    type string list*)
+let rec route_destination_list lst =
+  match lst with
+  | [] -> []
+  | h :: t -> h.destination :: route_destination_list t
+
+(** [get_possible_dates] is a recursive function that takes in the inputs
+    [route_from_list route_destination_list]. It checks if the specific
+    route_from and route_destination exists. if it does, return the list of
+    possible dates. if false, raise UnknownRoute exception.*)
+
+(** [check_date] takes in the inputs [date possible_dates]. It checks if the
+    specific date is in the list of possible dates. if false, raise UnknownRoute
+    exception*)
+
+(** [get_times] takes in the inputs [route date]. It returns the timeleave and
+    time arrive of the valide bus route.*)
+
+(** [get_price] takes in the inputs [route date time]. It returns the price and
+    the url to the bus ticket.*)
