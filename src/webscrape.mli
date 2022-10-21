30944a4d0bb874f5ce4f53ad9e774218b3b91928
Sydney Ho <sh967@cornell.edu>
2022-10-21 17:16:19 -0400

    Documented Webscrape module

--- src/webscrape.mli
+++ src/webscrape.mli
@@ -4,4 +4,8 @@
     scraping of web data from the megabus website. *)
 
 val require : string -> 'a option -> 'a
+(** [require] returns the search form message for a Megabus query *)
+
 val search : unit Mechaml.Agent.Monad.m
+(** [search] returns an updated state of a Mechaml Monad Agent for the HTTP GET
+    request and all valid forms of the selector message. *)
