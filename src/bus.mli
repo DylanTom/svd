214ca0b75bffd6bb05ea28ed82eee922acc86f28
Sydney Ho <sh967@cornell.edu>
2022-10-21 17:31:24 -0400

    Begin Bus module documentation

---
+++ src/bus.mli
@@ -0,0 +1,8 @@
+(** Representation of static bus route data.
+
+    This module represents the data stored in bus route files, including start
+    and end destinations, and time. It handles the loading of that data from
+    JSON as well as querying the data.
+
+    For examples, the specifications in this interface reference the example bus
+    query found in [test_data/bus.json].*)
